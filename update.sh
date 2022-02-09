6a12634b309437b4c7e594140cb07fb434520ad2
qazmoqwerty <sdonmeiri@gmail.com>
2022-02-09 02:24:52 +0200

    added current WIP dotfiles

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+function mkdir-clean() {
+    mkdir -p "$1"
+    rm -rf "$1"/*
+}
+
+mkdir-clean config/bspwm
+cp ~/.config/bspwm/bspwmrc config/bspwm
+
+mkdir-clean config/palette
+cp ~/.config/palette/palette.yml config/palette
+
+mkdir-clean config/picom
+cp ~/.config/picom/picom.conf config/picom
+
+mkdir-clean config/alacritty
+cp ~/.config/alacritty/alacritty.yml config/alacritty
+
+mkdir-clean config/micro
+cp ~/.config/micro/bindings.json config/micro
+cp ~/.config/micro/settings.json config/micro
+cp -r ~/.config/micro/colorschemes config/micro
+
+mkdir-clean config/feh
+cp ~/.config/feh/* config/feh
+
+mkdir-clean scripts/
+cp -r ~/.config/scripts/* scripts/
+
+mkdir-clean bin
+cp "$(which status-bar)" bin
+
+mkdir-clean home
+cp ~/.zshrc home/.zshrc
